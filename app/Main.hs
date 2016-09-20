bc3be8afad83673817117df71606c22603ae9b5d
Denis Igorevich Lobanov <denis.lobanov@zooei.com>
2016-09-20 09:06:10 +0100

    Moved to slack-api from Linkedlater
    Linkedlater is a better documented library, however it is for Slack
    integrations not bots. :(

--- app/Main.hs
+++ app/Main.hs
@@ -2,39 +2,30 @@
 
 module Main where
 
-import BasePrelude hiding (readFile)
-import Network.Linklater
+import Web.Slack
+import Web.Slack.Message
+
+import BasePrelude
+import Control.Monad
+import Data.Maybe (fromMaybe)
+import System.Environment (lookupEnv)
 import Data.Text (Text)
 import qualified Data.Text as Text
--- import GHC.Generics
-import Data.Text.IO (readFile)
-import Network.Wai.Handler.Warp (run, Port)
-import System.Environment
-import Control.Monad
+
+wrapToken :: String -> SlackConfig
+wrapToken token = SlackConfig { _slackApiToken = token }
+
+mindbot :: SlackBot()
+--mindbot (ImOpen uid imid) = do
+--    sendMessage imid $ "hello " <> show uid
+--    return()
+mindbot (Message cid usr msg _ _ _) = do
+    sendMessage cid $ "hello " <> (Text.pack (show usr))
+    return ()
 
 main :: IO ()
 main = do
-  url <-  liftM Text.pack $ getEnv "MINDBOT_TOKEN"
-  port <- liftM (\x -> read x::Port) $ getEnv "PORT"
-  putStrLn ("Listening on port " <> show port)
-  run port $ slashSimple (mindbot (Config url))
-
-mindbot :: Config -> Maybe Command -> IO Text
-mindbot config (Just command@(Command _ user channel _)) = do
-  putStrLn $ " Got command: " <> show command
-  say message config
-  return ""
-  where
-    message = greeting user channel
-
-mindbot config Nothing = do
-  say message config
-  return "No command given!"
-  where
-    message = SimpleMessage (EmojiIcon "wheelie") "mindbot" (GroupChannel "mindbot-testing") "Y I NO WORK?"
-
-greeting :: User -> Channel -> Message
-greeting user channel =
-  SimpleMessage (EmojiIcon "wheelie") "mindbot" channel $ "Hello " <> username
-  where
-    username = Text.pack $ show user
+  token <- fromMaybe (error "No $MINDBOT_TOKEN found in environment")
+            <$> lookupEnv "MINDBOT_TOKEN"
+  putStrLn "Mindbot running."
+  runBot (wrapToken token) mindbot ()
